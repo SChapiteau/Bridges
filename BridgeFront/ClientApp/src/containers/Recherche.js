0c45e6b556d1df277d1469b9a943184bf8921253
Stephane.C <tchadizman@gmail.com>
2020-06-17 18:42:22 +0200

    Affichage conditionnel du resultat de recherche

--- BridgeFront/ClientApp/src/containers/Recherche.js
+++ BridgeFront/ClientApp/src/containers/Recherche.js
@@ -3,34 +3,54 @@ import React, { Component } from 'react';
 export class Recherche extends Component {
     static displayName = Recherche.name;
 
+    
+
     constructor(props) {
         super(props);
-        this.state = { };
+        this.state = {
+            NomEntreprise: '',
+            items : []
+        };
 
         this.rechercher = this.rechercher.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.updateNomEntreprise = this.updateNomEntreprise.bind(this);
     }
 
-    handleChange(event) {
+    updateNomEntreprise(event) {
         this.setState({ NomEntreprise: event.target.value });
-        console.log(event.target.value)
     }
 
     rechercher(event) {
         alert('Le nom a été soumis : ' + this.state.NomEntreprise);
+        this.setState({
+            items: ["item1", "item2", "item3"
+                //{ "id": 1, "name": "Apples", "price": "$2" },
+                //{ "id": 2, "name": "Peaches", "price": "$5" }
+            ]
+        });
+        console.log(this.state.items)
+
         event.preventDefault();
     }
 
-    render () {
-    return (
-            <div>
-            <h1>Recherche</h1>
-            <form onSubmit={this.rechercher}>
-                <label> Nom entreprise : <input type="text" value={this.state.NomEntreprise} onChange={this.handleChange}/> </label>
-                <input type="submit" value="Envoyer" />
-            </form>
+    render() {
 
-            <button className="btn btn-primary" onClick={this.rechercher}>Rechercher</button>
+        var resultat;
+        if (this.state.items.length > 0) {
+            resultat = <div>resultat de recherche</div>
+        }
+
+
+        return (
+            <div>
+                <h1>Recherche</h1>
+                <form onSubmit={this.rechercher}>
+                    <label> Nom entreprise : <input type="text" value={this.state.NomEntreprise} onChange={this.updateNomEntreprise} /> </label>
+                    <p><button className="btn btn-primary" onClick={this.rechercher}>Rechercher</button></p>
+                </form>
+
+                {resultat}
+            
             </div>
         );
     }
