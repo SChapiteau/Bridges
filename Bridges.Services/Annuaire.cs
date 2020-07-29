3a54cbd33f6c8105af5105ad35ad64fca61973fe
Stephane.C <tchadizman@gmail.com>
2020-07-29 17:22:03 +0200

    Creatuion d'un test unitaire

--- Bridges.Services/Annuaire.cs
+++ Bridges.Services/Annuaire.cs
@@ -7,17 +7,25 @@ namespace Bridges.Services
 {
     public class Annuaire
     {
+        private readonly IEntrepriseRepository enterpriseRepository;
+
+        public Annuaire(IEntrepriseRepository enterpriseRepository)
+        {
+            this.enterpriseRepository = enterpriseRepository;
+        }
        public IEnumerable<Entreprise> GetAll()
         {
-            //Faire appel a un IEnterpriseRepository
-            IEnumerable<Entreprise> result = new List<Entreprise>
-            {
-                new Entreprise{SIRET = "123", Nom="Entreprise 1"},
-                new Entreprise{SIRET = "222", Nom="Entreprise 2"},
-                new Entreprise{SIRET = "333", Nom="Entreprise 3"}
-            };
+            //IEnumerable<Entreprise> result = new List<Entreprise>
+            //{
+            //    new Entreprise{SIRET = "123", Nom="Entreprise 1"},
+            //    new Entreprise{SIRET = "222", Nom="Entreprise 2"},
+            //    new Entreprise{SIRET = "333", Nom="Entreprise 3"}
+            //};
+            //return result;
 
-            return result;
+            return enterpriseRepository.GetAll();
+
+            
         }
     }
 }
